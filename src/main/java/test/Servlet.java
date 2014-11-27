7793b5aa7c841e1f921ca52299888b3d50129bea
xaos <xaos@z-inc.net>
2014-11-27 18:34:44 +0500

    FIX

--- src/main/java/test/Servlet.java
+++ src/main/java/test/Servlet.java
@@ -8,19 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getRequestURI().equals("/")) {
             InputStream input = getServletContext().getResourceAsStream("/templates/index.html");
-            resp.getWriter().write(IOUtils.toString(input));
+            resp.getOutputStream().write(IOUtils.toString(input).getBytes(Charset.defaultCharset()));
         } else if (req.getRequestURI().equals("/runtime.js")) {
             InputStream input = getServletContext().getResourceAsStream("/js/teavm/runtime.js");
-            resp.getWriter().write(IOUtils.toString(input));
+            resp.getOutputStream().write(IOUtils.toString(input).getBytes(Charset.defaultCharset()));
         } else if (req.getRequestURI().equals("/classes.js")) {
             InputStream input = getServletContext().getResourceAsStream("/js/teavm/classes.js");
-            resp.getWriter().write(IOUtils.toString(input));
+            resp.getOutputStream().write(IOUtils.toString(input).getBytes(Charset.defaultCharset()));
         }
     }
 }
